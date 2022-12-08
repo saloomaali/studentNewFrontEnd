7f6adf1939ba9f709f38133765e5299cfdb9d9d3
Salooma <saloomaali2000@gmail.com>
2022-12-08 17:09:40 +0530

    frontend completed

---
+++ src/app/api.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient){}
+
+  fetchStudent=()=>{
+
+    return this.http.get("http://localhost:8080/view")
+  }
+
+  addStudent=(dataToSend:any)=>{
+    return this.http.post("http://localhost:8080/add",dataToSend)
+  }
+
+  searchStudent=(dataToSend:any)=>{
+    return this.http.post("http://localhost:8080/search",dataToSend)
+  }
+
+  deleteStudent =(dataToSend:any)=>{
+    return this.http.post("http://localhost:8080/delete",dataToSend)
+  }
+
+}
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-search',
@@ -6,5 +7,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
+
+  admnNo = ""
+
+  constructor(private api:ApiService){}
+  searchData:any = []
+  readValue=()=>{
+    
+    
+    let data:any = {"admnNo": this.admnNo}
+    this.api.searchStudent(data).subscribe(
+
+      (response:any)=>{
+        if(response.length == 0){
+
+          alert("There is no such students")
+          
+        }
+        else{
+
+          this.searchData = response
+          
+          
+        }
+      }
+    )
+    
+
+  }
+  deletebtn=(id:any)=>{
+    let data: any = {"id": id}
+
+    this.api.deleteStudent(data).subscribe(
+
+      (response:any)=>{
+
+        console.log(response)
+
+        if (response.status == "success") {
+
+          alert("Student deleted successfully")
+          window.location.reload()
+          
+        } 
+      }
+    )
+  }
 
 }
